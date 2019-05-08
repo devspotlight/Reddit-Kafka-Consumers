e57e633caca459fbf06d4ed7c4124993c8d7b6b6
Jorge Orpinel <jorge@orpinel.com>
2019-05-08 01:44:59 -0400

    Removes all `;` (semicollon) symbols.

--- redshift_batch/index.js
+++ redshift_batch/index.js
@@ -1,13 +1,14 @@
 const Postgres = require('pg-promise')({
     capSQL: true
-});
-const Config = require('getconfig');
-const Kafka = require('no-kafka');
-const { performance } = require('perf_hooks');
+})
+const Config = require('getconfig')
+const Kafka = require('no-kafka')
+const { performance } = require('perf_hooks')
 
-const db = Postgres(Config.database);
-db.connect();
-const commentsTable = new Postgres.helpers.ColumnSet([
+const db = Postgres(Config.database)
+db.connect()
+// console.debug('Connected to', Config.database, 'db') // NOTE: This output contains secrets
+const commentsColumns = new Postgres.helpers.ColumnSet([
   'author_link_karma',
   'author_comment_karma',
   'author_created_at',
@@ -47,55 +48,60 @@ const commentsTable = new Postgres.helpers.ColumnSet([
   'is_troll',
   'recent_comments',
   'is_training'
-], {table: 'reddit_comments'});
+], {table: 'reddit_comments'})
 
 const consumer = new Kafka.SimpleConsumer({
   ...Config.kafka.config,
   groupId: Config.kafka.group
-});
+})
 
-let queue = [];
-let lastUpdate = performance.now();
-let lock = false;
+let queue = []
+let lastUpdate = performance.now()
+let lock = false
 
 const dataHandler = (messageSet, topic, partition) => {
   console.debug('redshift_batch: handling', messageSet.length, 'messages from', topic, partition)
   messageSet.forEach((msg) => {
-    const now = performance.now();
-    const sinceLast = now - lastUpdate;
-    const value = JSON.parse(msg.message.value);
+    const now = performance.now()
+    const sinceLast = now - lastUpdate
+    const value = JSON.parse(msg.message.value)
     console.debug('redshift_batch:   queueing comment', value.link_id, value.created_utc)
-    const offset = msg.offset;
+    const offset = msg.offset
     // console.debug('redshift_batch:   processing msg at', offset)
-    const length = queue.push(value);
+    const length = queue.push(value)
     // console.debug('redshift_batch: queue length', length)
 
-    // Executes a `query` with the cumulative `queue` of row values every time the db is not locked.
+    // TODO: console.info only if NODE_ENV !== 'production'
+    // Executes a `query` with the cumulative `queue` of row values every time the below process is not locked.
     if (lock === false && (length >= Config.queueSize || sinceLast > Config.timeout)) {
-      console.debug('redshift_batch:   inserting', queue.length, 'message rows to Redshift');
-      lock = true;
-      lastUpdate = now;
-      const query = Postgres.helpers.insert(queue, commentsTable);
+      console.debug('redshift_batch:   inserting', queue.length, 'message row(s) to db')
+      // console.debug('redshift_batch:   messages to insert:\n', queue)
+      lock = true
+      lastUpdate = now
+      // See http://vitaly-t.github.io/pg-promise/helpers.html#.insert
+      const query = Postgres.helpers.insert(queue, commentsColumns)
+      // console.debug('redshift_batch:   insert query:', query)
       db.query(query, queue)
         .then(() => {
-          return consumer.commitOffset({ topic, partition, offset });
+          return consumer.commitOffset({ topic, partition, offset })
+          // NOTE: A committed offset indicates that all messages up to this offset have been processed.
         })
         .then(() => {
-          lock = false;
-          // console.debug('redshift_batch:   unlock');
+          lock = false
+          // console.debug('redshift_batch:   unlock')
         })
         .catch((err) => {
-          lock = false;
-          // console.debug('redshift_batch:   err', err);
-        });
-      queue = [];
+          lock = false
+          console.error('redshift_batch:   db query error!', err)
+        })
+      queue = []
     }
     // TODO: What if the db is locked during the last n messages when the loop ends? Will those rows get inserted?
-  });
-};
+  })
+}
 
 
 consumer.init().then(() => {
-  consumer.subscribe(Config.kafka.topic, dataHandler);
-});
+  consumer.subscribe(Config.kafka.topic, dataHandler)
+})
 
